4254e59d1eab049c23f8299246640702cb1ce5c1
Egor Mikhailov <egor11022004@gmail.com>
2023-11-07 11:46:47 +0300

    Initial Commit

---
+++ WordGuesser/WordGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGuesserApp.swift
+//  WordGuesser
+//
+//  Created by Егор Михайлов on 07.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
