d71aa557cc25eaba59d1321f7709931df723d81b
Egor Mikhailov <egor11022004@gmail.com>
2023-11-10 13:38:56 +0300

    Added DefinitionView

---
+++ WordGuesser/Words/Views/DefinitionView.swift
@@ -0,0 +1,8 @@
+//
+//  DefinitionView.swift
+//  WordGuesser
+//
+//  Created by Егор Михайлов on 10.11.2023.
+//
+
+import Foundation
---
+++ WordGuesser/Words/WordsDefiner/WordDefinitionResponse.swift
@@ -0,0 +1,8 @@
+//
+//  WordDefinitionResponse.swift
+//  WordGuesser
+//
+//  Created by Егор Михайлов on 10.11.2023.
+//
+
+import Foundation
